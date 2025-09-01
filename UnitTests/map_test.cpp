03f65736b2985ae8c298dfeb3c051bd0300e7b67
Andrew <andrew.schroed@gmail.com>
2025-09-01 21:59:28 +0200

    Added map_test.cpp for future unit tests

---
+++ UnitTests/map_test.cpp
@@ -0,0 +1,16 @@
+//
+// Created by andrew on 2025-09-01.
+//
+
+
+#include <dai/map.h>
+#include <vector>
+
+
+using namespace dai;
+
+#define BOOST_TEST_MODULE MapTest
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
